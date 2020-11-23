01637f5d9aefd5fbbbac6c377ac9ea4ecb2767d2
leandro0104 <leandro.asepulveda@gmail.com>
2020-11-23 15:13:38 -0300

    Se agrega y habilita cors en controller

--- src/main/java/cl/altiuz/academic/controllers/AcademicController.java
+++ src/main/java/cl/altiuz/academic/controllers/AcademicController.java
@@ -3,19 +3,21 @@ package cl.altiuz.academic.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import cl.altiuz.academic.entity.Academic;
 import cl.altiuz.academic.service.IAcademicService;
 
 @RestController
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
 public class AcademicController {
 	
 	@Autowired
 	private IAcademicService academicService;
-	
 	@GetMapping("/listAcademic")
 	public List<Academic> listAcademic(@RequestHeader String rut){
 		return academicService.findbyRut(rut);
