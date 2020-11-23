c762d3da76e534b76f0fdfba944423b9e8a9210f
leandro0104 <leandro.asepulveda@gmail.com>
2020-11-23 11:22:28 -0300

    Se agrega configuracion para deploy en otro servidor

--- src/main/java/cl/altiuz/academic/PocAcademicoApplication.java
+++ src/main/java/cl/altiuz/academic/PocAcademicoApplication.java
@@ -2,10 +2,17 @@ package cl.altiuz.academic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PocAcademicoApplication {
+public class PocAcademicoApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(PocAcademicoApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PocAcademicoApplication.class, args);
 	}
